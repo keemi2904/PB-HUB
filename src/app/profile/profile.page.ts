a14ab27d7ebd12e3d722f0da06675331a9061b46
HakeemAzeez <22ftt1511@student.pb.edu.bn>
2024-04-06 13:32:05 +0800

    edited profile page + logout

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonBackButton, IonCard, IonCardContent } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonBackButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { cartOutline, informationCircleOutline, chatboxEllipsesOutline, logOutOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonButtons, IonBackButton, IonCard, IonCardContent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonButtons, IonBackButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonIcon]
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({ cartOutline, informationCircleOutline, chatboxEllipsesOutline, logOutOutline});
+  }
 
   ngOnInit() {
   }
