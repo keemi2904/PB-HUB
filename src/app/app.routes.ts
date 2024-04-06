0a7fe23fb46161f02c3a1ea42550526aa9343a00
HakeemAzeez <22ftt1511@student.pb.edu.bn>
2024-04-06 11:38:13 +0800

    added login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,34 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    redirectTo: '/auth',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('./auth/auth.page').then( m => m.AuthPage)
+  },
+  {
+    path: 'tabs',
+    loadComponent: () => import('./tabs/tabs.page').then( m => m.TabsPage),
+    children: [
+      {
+        path: '',
+        redirectTo: '/tabs/tab1',
+        pathMatch: 'full'
+      },
+      {
+        path: 'tab1',
+        loadComponent: () => import('./tab1/tab1.page').then( m => m.Tab1Page)
+      },
+      {
+        path: 'tab2',
+        loadComponent: () => import('./tab2/tab2.page').then( m => m.Tab2Page)
+      },
+      {
+        path: 'tab3',
+        loadComponent: () => import('./tab3/tab3.page').then( m => m.Tab3Page)
+      },
+    ]
   },
 ];
